33ba42a16ec0d5ad1e66ed6c1750a1e9b2a60d49
Jonathan Joel Anchundia Navas <joel.an_92@hotmail.com.ar>
2022-11-20 19:59:45 -0500

    Validación para enviar datos de formulario Login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,34 @@
 function Login(params) {
+    const submitHandle = (e)=>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (email === "" || password === "") {
+            console.log("Los campos no deben estar vacios");
+            return;
+        }
+
+        if (email !== "" && !regexEmail.test(email)) {
+            console.log("Escribir un correo correcto");
+            return;
+        }
+
+        if (email !== "jjanchundia92@gmail.com" || password !== "12345") {
+            console.log("Credenciales incorrectas");
+        }
+
+        console.log("Ok, estamos listo para enviar la información");
+    }
+
   return (
     <div>
       <h2>Formulario de Login</h2>
-      <form>
+      <form onSubmit={submitHandle}>
         <label>
           <span>Correo Electrónico:</span>
-          <input type="email" name="email" />
+          <input type="text" name="email" />
           <br />
         </label>
         <label>
