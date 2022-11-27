6638802097b6f38e9b5c8a49b37699adcf80224b
Jonathan Joel Anchundia Navas <joel.an_92@hotmail.com.ar>
2022-11-27 10:03:17 -0500

    Creación de Navbar y Footer e implementación en nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Listado from "./components/Listado";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 //El Switch se lo sustituye por Routes en react-router-dom v6
 function App() {
   return (
     <>
+    <Header/>
       {/* Redireccionamiento de url */}
       {/* <Route path="/" component={<Login />} /> asi se usaba antes */}
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route path="/listado" element={<Listado />} />
       </Routes>
+      <Footer/>
     </>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+
+function Footer() {
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/" rel="noopener noreferrer">Facebook</a>
+                    </li>
+                </ul>
+            </nav>
+            <p>Copyright Alkemy Challenger</p>
+        </footer>
+    );
+}
+
+export default Footer;
