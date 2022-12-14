3f03583e9b859217815a4cefeefbdd1cb1819a1c
Jonathan Joel Anchundia Navas <joel.an_92@hotmail.com.ar>
2022-12-13 19:40:22 -0500

    Renderización de los datos en el componente, se muestra los datos del api en nuestro proyecto

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,18 +4,36 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/listado">Listado</Link>
-          </li>
-          <li>
-            <Link to="/contacto">Contacto</Link>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            jjanchundia
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/listado">Listado</Link>
+              </li>
+              {/* <li>
+                <Link to="/contacto">Contacto</Link>
+              </li> */}
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
--- src/Components/Listado.js
+++ src/Components/Listado.js
@@ -9,40 +9,43 @@ function Listado() {
   const [moviesList, setMoviesList] = useState([]);
 
   useEffect(() => {
-    const endPoint = "https://api.themoviedb.org/3/discover/movie?api_key=cf80d99a3562682f7e670df7df66f082&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+    const endPoint =
+      "https://api.themoviedb.org/3/discover/movie?api_key=cf80d99a3562682f7e670df7df66f082&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
     axios.get(endPoint)
-    .then((res) => {
+    .then( res => {
       const apiData = res.data;
-      setMoviesList(apiData);
+      setMoviesList(apiData.results);
     });
   }, [setMoviesList]);
 
-  console.log(moviesList)
+  console.log(moviesList);
 
   return (
     <>
-    {/* Validamos de q el token no sea vacio para redireccionar */}
-    {/* Para proteger nuestra ruta */}
-    { !token && <Navigate to="/" />}
+      {/* Validamos de q el token no sea vacio para redireccionar */}
+      {/* Para proteger nuestra ruta */}
+      {!token && <Navigate to="/" />}
 
-    <div className="row">
-    {/* estructura base */}
-      <div className="col-3">
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Movie title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <Link href="/" className="btn btn-primary">
-              View Details
-            </Link>
-          </div>
-        </div>
+      <div className="row">
+        {/* estructura base */}
+        {
+          moviesList.map((oneMovie, index) => {
+          return (
+            <div className="col-3" key={index}>
+              <div className="card my-4">
+                <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{oneMovie.title.substring(0, 20)}...</h5>
+                  <p className="card-text">{oneMovie.overview.substring(0, 100)}...</p>
+                  <Link href="/" className="btn btn-primary">
+                    View Details
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </div>
     </>
   );
 }
