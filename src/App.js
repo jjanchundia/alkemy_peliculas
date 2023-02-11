77b5c1822be0c1a8a57c7fe8ebaf4157c3d6d731
Jonathan Joel Anchundia Navas <joel.an_92@hotmail.com.ar>
2023-02-11 15:59:40 -0500

    Se agrega logica para agregar a favoritos

--- src/App.js
+++ src/App.js
@@ -11,24 +11,70 @@ import Resultado from "./components/Resultados";
 
 // Estilos
 import "./css/bootstrap.min.css";
-// import "./css/app.css";
+import "./css/app.css";
 
 //El Switch se lo sustituye por Routes en react-router-dom v6
 function App() {
+  const favMovies = localStorage.getItem("favs");
+  let tempMoviesInFavs;
+
+  if (favMovies === null) {
+    tempMoviesInFavs = [];
+  } else {
+    tempMoviesInFavs = JSON.parse(favMovies);
+  }
+
+  const addOrRemoveFavorite = (e) => {
+    const btn = e.target;
+    // capturamos elemento padre(div) del boton
+    const parent = btn.parentElement;
+    const imgURL = parent.querySelector("img").getAttribute("src");
+    const title = parent.querySelector("h5").innerText;
+    const overView = parent.querySelector("p").innerText;
+    const movieData = {
+      imgURL: imgURL,
+      title: title,
+      overView: overView,
+      id: btn.dataset["movieId"],
+      // asi obtenemos el id del boton
+      // btn.dataset["movieId"]
+    };
+    // console.log(movieData);
+    // console.log(btn.dataset["movieId"]);
+    // filtramos los id seleccionado con los q ya estan en el array
+    let movieIsInArray = tempMoviesInFavs.find(x => {
+      return x.id === movieData.id;
+    });
+
+    if (!movieIsInArray) {
+      tempMoviesInFavs.push(movieData);
+      localStorage.setItem("favs", JSON.stringify(tempMoviesInFavs));
+    }else{
+      // Aqui eliminamos la pelicula de favorito repetida
+      let moviesLeft = tempMoviesInFavs.filter(x=>{
+        return x.id != movieData.id;
+      });
+      localStorage.setItem("favs", JSON.stringify(moviesLeft));
+    }
+  };
+
   return (
     <>
-    <Header/>
+      <Header />
       {/* Redireccionamiento de url */}
       {/* <Route path="/" component={<Login />} /> asi se usaba antes */}
       <div className="container mt-3">
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route path="/listado" element={<Listado />} />
-        <Route path="/detalle" element={<Detalle />} />
-        <Route path="/resultado" element={<Resultado />} />
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route
+            path="/listado"
+            element={<Listado addOrRemoveFavorite={addOrRemoveFavorite} />}
+          />
+          <Route path="/detalle" element={<Detalle />} />
+          <Route path="/resultado" element={<Resultado />} />
+        </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/Components/Listado.js
+++ src/Components/Listado.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { useNavigate, Link, Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 import sweetAlert from '@sweetalert/with-react';
 
-function Listado() {
+function Listado(props) {
+  console.log(props)
+  // alert(props);
   // const history = useNavigate();
   // Redirect ahora es Navigate
   let token = sessionStorage.getItem("token");
@@ -38,6 +40,9 @@ function Listado() {
             <div className="col-3" key={index}>
               <div className="card my-4">
                 <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="..." />
+                {/* ejecutamos funcion q recibimos por las props */}
+                {/*  data-movie-id={oneMovie.id} para capturar el id */}
+                <button className="favorite-btn" onClick={ props.addOrRemoveFavorite } data-movie-id={oneMovie.id}>🖤</button>
                 <div className="card-body">
                   <h5 className="card-title">{oneMovie.title.substring(0, 20)}...</h5>
                   <p className="card-text">{oneMovie.overview.substring(0, 100)}...</p>
